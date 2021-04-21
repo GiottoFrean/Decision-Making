78f707293938df5e9014cfdd7c7831abcda144dd
Giotto Frean <gmfrean@gmail.com>
2021-04-21 23:41:40 +1200

    starting chapter 3

--- modules/factors.py
+++ modules/factors.py
@@ -58,7 +58,7 @@ def condition(factor,axis="none"):
 			for i in range(indexes.shape[0]):
 				norm = sums[tuple(indexes[i,cond_var_index])]
 				new_factor.set(indexes[i],array[tuple(indexes[i])]/norm)
-	return new_factor
+			return new_factor
 
 def marginalize(factor,axis="none"):
 	array = factor.array
@@ -77,7 +77,7 @@ def marginalize(factor,axis="none"):
 			new_names = [names[n] for n in not_marg_var_index]
 			new_factor = Factor(new_names,summed_array.shape)
 			new_factor.set_all(summed_array)
-	return new_factor
+			return new_factor
 	
 def product(factor1,factor2):
 	names1 = factor1.names
@@ -93,6 +93,31 @@ def product(factor1,factor2):
 		f2_part = i[from_2_to_new_index]
 		new_factor.set(i,factor1.get(f1_part)*factor2.get(f2_part))
 	return new_factor
+
+def cut_variables(factor,axis,values):
+	array = factor.array
+	indexes = factor.indexes
+	names = factor.names
+	if(len(axis)==len(factor.names)):
+		return None
+	else:
+		var_index = [a for a in np.arange(len(names)) if names[a] in axis]
+		if(len(var_index)<1):
+			print("Error: couldn't find variable")
+			return None
+		else:
+			not_var_index = [b for b in range(len(names)) if not b in var_index]
+			slc = [slice(None)]*len(names)
+			for v_i in range(len(var_index)):
+				slc[var_index[v_i]]=slice(values[v_i],values[v_i]+1)
+			sliced_array = np.squeeze(array[tuple(slc)])
+			new_names = [names[n] for n in not_var_index]
+			new_factor = Factor(new_names,sliced_array.shape)
+			new_factor.set_all(sliced_array) # squeeze removes all axes with 1 dim.
+			return new_factor
+
+
+
 
 
 
