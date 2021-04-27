c5f93fb43427c33858306aedc46a8885dcfcad90
Giotto Frean <gmfrean@gmail.com>
2021-04-28 00:09:22 +1200

    adding comments to modules, adding factor sampling. Fixing nb2 ch3

--- modules/exact_inference.py
+++ modules/exact_inference.py
@@ -1,38 +1,49 @@
 import factors
 
+# SUM PRODUCT
+# This runs the sum product algorithm for variable elimination. 
+# Every name in known_vars has a piece of evidence associated with it.
+# Every variable in unknown_vars is marginalized. Returns a joint distribution over what is left.
+# The order of unknown_vars is the order in which marginalization happens.
+
 def sum_product_variable_elimination(all_factors,known_vars,evidence,unknown_vars):
-    new_factors = []
-    for f in all_factors:
-        deleted_f = factors.drop_variables(f,known_vars,evidence)
-        if(deleted_f!=None):
-            new_factors.append(deleted_f)
-    
-    for unknown_var in unknown_vars:
-        factors_to_combine = []
-        factors_to_exclude = []
-        for f in new_factors:
-            if(unknown_var in f.names):
-                factors_to_combine.append(f)
-            else:
-                factors_to_exclude.append(f)
-        new_factors = factors_to_exclude
-        if(len(factors_to_combine)>0):
-            combined_factor = factors.multiple_factor_product(factors_to_combine)
-            combined_factor = factors.marginalize(combined_factor,[unknown_var])
-            if(not isinstance(combined_factor,(int,float))):
-                new_factors.append(combined_factor)
-    if(len(new_factors)>1):
-        combined_factor = factors.multiple_factor_product(new_factors)
-        new_factors = [combined_factor]
-    return factors.condition(new_factors[0])
+	# Step 1: condition by factor by setting each known variable to each piece of evidence
+	new_factors = []
+	for f in all_factors:
+		deleted_f = factors.drop_variables(f,known_vars,evidence)
+		if(deleted_f!=None):
+			new_factors.append(deleted_f)
+	
+	# Step 2: marginalize all unknown variables. This requires merging all factors with the same variable name.
+	for unknown_var in unknown_vars:
+		factors_to_combine = []
+		factors_to_exclude = []
+		for f in new_factors:
+			if(unknown_var in f.names):
+				factors_to_combine.append(f)
+			else:
+				factors_to_exclude.append(f)
+		# left over factors are the ones without the unknown variable and the marginalized product
+		new_factors = factors_to_exclude
+		if(len(factors_to_combine)>0):
+			combined_factor = factors.multiple_factor_product(factors_to_combine)
+			combined_factor = factors.marginalize(combined_factor,[unknown_var])
+			# If the resulting table is a single number, then all variables were marginalized, which means total independence, so ignore.
+			if(not isinstance(combined_factor,(int,float))):
+				new_factors.append(combined_factor)
+				
+	# merge all remaining factors
+	final_combined_factor = factors.multiple_factor_product(new_factors)
+	final_normalized_factor = factors.condition(final_combined_factor)
+	return final_normalized_factor
 
 def full_joint_elimination(all_factors,known_vars,evidence,unknown_vars):
-    full_joint_factor = factors.multiple_factor_product(all_factors)
-    set_vars = factors.drop_variables(full_joint_factor,known_vars,evidence)
-    marginalized = factors.marginalize(set_vars,unknown_vars)
-    if(marginalized!=None):
-        normalized = factors.condition(marginalized)
-        return normalized
-    else:
-        normalized = factors.condition(set_vars)
-        return normalized
+	full_joint_factor = factors.multiple_factor_product(all_factors)
+	set_vars = factors.drop_variables(full_joint_factor,known_vars,evidence)
+	marginalized = factors.marginalize(set_vars,unknown_vars)
+	if(marginalized!=None):
+		normalized = factors.condition(marginalized)
+		return normalized
+	else:
+		normalized = factors.condition(set_vars)
+		return normalized
--- modules/factors.py
+++ modules/factors.py
@@ -1,11 +1,25 @@
 import numpy as np
 
+# This is code to run factors in numpy.
+# The class is fairly simple, just contains:
+# 1. the array which has the values at each possible combination of variables. One dimension for each variable.
+# 		e.g [[0.2,0.4]
+#			 [0.1,0.3]]
+# 2. the index which has the index of each combination of variables. This is a 2d array which lists all possible variable combinations.
+# 		e.g [[0,0]
+#			 [0,1]
+#			 [1,0]
+#			 [1,1]]
+# 3. the names of each variable e.g ["A","B"]
+# The reason for having the index is it makes it easier to edit values and easier to print the factor.
+
 class Factor:
 	def __init__(self,names,pos_values):
 		array = np.zeros((pos_values))
 		indexes = np.zeros((np.prod(array.shape),len(names))).astype(int)
 		f = 1
 		for i in range(len(names)):
+			# fills the given column in the indexes
 			repeated = np.arange(array.shape[i]).repeat(np.prod(array.shape)/(f*array.shape[i]))
 			indexes[:,i]=np.tile(repeated,f)
 			f = f*array.shape[i]
@@ -13,6 +27,7 @@ class Factor:
 		self.array = array
 		self.indexes = indexes
 		
+	# show the array in a nice format.
 	def __repr__(self):
 		array = self.array
 		indexes = self.indexes
@@ -26,6 +41,7 @@ class Factor:
 			strings.append(formatter.format(*(list(indexes[i])+[val])))
 		return "".join([s+"\n" for s in strings])
 		
+	# set the value at a given position e.g myfactor.set([0,1],0.3)
 	def set(self,index,value):
 		ndim = len(self.names)
 		if(len(index)==ndim):
@@ -33,16 +49,23 @@ class Factor:
 		else:
 			raise Exception('length of index is incorrect. Provide {} values'.format(ndim))
 
+	# get the value at a given index e.g myfactor.get([0,1])
 	def get(self,index):
 		return self.array[tuple(index)]
 	
+	# just sets all the values in the same order as printed. Useful when testing and doing things in as few lines as possible. 
 	def set_all(self,values):
 		self.array = np.array(values).reshape(self.array.shape,order="C")
 		
+# There are four major pieces of code to know:
+
+# 1. FACTOR CONDITIONING
+# 		returns the same factor but ensures the sum is 1 along a given set of axes.	
 def condition(factor,axis="none"):
 	array = factor.array
 	indexes = factor.indexes
 	names = factor.names
+	# if axis is none, then normalize the whole array so the total probability is 1.
 	if(axis=="none"):
 		new_factor = Factor(names,array.shape)
 		new_factor.set_all(array/np.sum(array))
@@ -53,14 +76,19 @@ def condition(factor,axis="none"):
 			print("Error: couldn't find variable")
 			return None
 		else:
+			# find all variables not in the axis list and get their sum. This is easy in numpy. 
+			# Gets the array created by summing out variables not in the axis list.
 			not_cond_var_index = [b for b in range(len(names)) if not b in cond_var_index]
 			sums = np.sum(array,axis=tuple(not_cond_var_index))
+			# make a new factor which is the same but divided by the normalization.
 			new_factor = Factor(names,array.shape)
 			for i in range(indexes.shape[0]):
 				norm = sums[tuple(indexes[i,cond_var_index])]
 				new_factor.set(indexes[i],array[tuple(indexes[i])]/norm)
 			return new_factor
 
+# 2. FACTOR MARGINALIZATION
+# 		returns a smaller factor, taking the expectation over all variables in axis
 def marginalize(factor,axis="none"):
 	array = factor.array
 	indexes = factor.indexes
@@ -72,6 +100,7 @@ def marginalize(factor,axis="none"):
 		if(len(marg_var_index)<1):
 			return None
 		else:
+			# fairly simple, just sum out the variables in the axis list.
 			not_marg_var_index = [b for b in range(len(names)) if not b in marg_var_index]
 			summed_array = np.sum(array,axis=tuple(marg_var_index))
 			new_names = [names[n] for n in not_marg_var_index]
@@ -79,26 +108,36 @@ def marginalize(factor,axis="none"):
 			new_factor.set_all(summed_array)
 			return new_factor
 
+# 3. FACTOR PRODUCT
+# 		returns a joined factor. Most complicated piece of code here. Book gives good visualization.
+#		for each variable in factor1 which is not also in factor2 it needs to go through every option in factor2.
+
 def product(factor1,factor2):
 	names1 = factor1.names
 	names2 = factor2.names
+	# find variables both in factor 1 and 2
 	joint_names = [n for n in names1 if n in names2]
+	# get a list of all variables which will be in the new factor
 	new_names = names1 + [n for n in names2 if not n in joint_names]
 	from_2_to_new_index = [new_names.index(n) for n in names2]
 	factor2_not_joint_index = [n for n in range(len(names2)) if not names2[n] in joint_names]
+	# get the shape of the new array. Essentially adding new dimensions to factor1
 	new_shapes = list(factor1.array.shape)+[factor2.array.shape[n] for n in factor2_not_joint_index]
 	new_factor = Factor(new_names,new_shapes)
+	# every option in the new factor is the value in both factors with their respective variables multiplied.
 	for i in new_factor.indexes:
 		f1_part = i[:len(names1)]
 		f2_part = i[from_2_to_new_index]
 		new_factor.set(i,factor1.get(f1_part)*factor2.get(f2_part))
 	return new_factor
 
-def multiple_factor_product(all_factors):
-	joint_factor = all_factors[0]
-	for i in range(1,len(all_factors)):
-		joint_factor = product(joint_factor,all_factors[i])
-	return joint_factor
+# 4. DROP VARIABLES
+#		selects a variables at particular values. 
+#		E.g if the array is:
+#		[[0.2,0.4]
+#		 [0.1,0.3]]
+# 		and I select only row 1, then the new factor is:
+#		[0.1,0.3] (1d now instead of 2d)
 
 def drop_variables(factor,axis,values):
 	array = factor.array
@@ -120,7 +159,22 @@ def drop_variables(factor,axis,values):
 		new_factor.set_all(sliced_array) # squeeze removes all axes with 1 dim.
 		return new_factor
 
+# simple code to do factor multiplication for a list of factors
+def multiple_factor_product(all_factors):
+	joint_factor = all_factors[0]
+	if(len(all_factors)==1):
+		return joint_factor
+	for i in range(1,len(all_factors)):
+		joint_factor = product(joint_factor,all_factors[i])
+	return joint_factor
 
+# simple code to sample variables from the factor.
+def sample(factor,number_of_samples):
+	array = factor.array
+	indexes = factor.indexes
+	normalized_array = array/np.sum(array)
+	rows = np.random.choice(np.arange(indexes.shape[0]),number_of_samples,p=normalized_array.reshape(-1))
+	return indexes[rows]
 
 
 
