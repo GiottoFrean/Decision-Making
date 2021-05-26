7d884410461c08455a172297ba98ba5c8683e6e0
Giotto Frean <gmfrean@gmail.com>
2021-05-26 18:56:42 +1200

    done with Ch5. Just made a small notebook but not going into much depth

--- modules/factors_inference.py
+++ modules/factors_inference.py
@@ -1,6 +1,19 @@
 import factors
 import numpy as np
 
+def get_log_likelihood(all_factors,known_vars,evidence):
+	prob = 0
+	for f in all_factors:
+		f_known_values = [evidence[known_vars.index(name)] for name in f.names if name in known_vars]
+		f_unknown_vars = [name for name in f.names if not name in known_vars]
+		if(len(f_unknown_vars)>0):
+			f_marg = factors.marginalize(f,f_unknown_vars)
+			if(not isinstance(f_marg,(int,float))):
+				prob+=np.log(f_marg.get(f_known_values))
+		else:
+			prob+=np.log(f.get(f_known_values))
+	return prob
+
 # SUM PRODUCT
 # This runs the sum product algorithm for variable elimination. 
 # Every name in known_vars has a piece of evidence associated with it.
@@ -39,7 +52,6 @@ def sum_product_variable_elimination(all_factors,known_vars,evidence,unknown_var
 	return final_normalized_factor
 
 # Does variable elimination by constructing full factor
-
 def full_joint_elimination(all_factors,known_vars,evidence,unknown_vars):
 	full_joint_factor = factors.multiple_factor_product(all_factors)
 	set_vars = factors.drop_variables(full_joint_factor,known_vars,evidence)
@@ -51,34 +63,39 @@ def full_joint_elimination(all_factors,known_vars,evidence,unknown_vars):
 		normalized = factors.condition(set_vars)
 		return normalized
 
-# Learns a directed model MLE parameters, using the 
-def learn_directed_PGM_EM(prior_factors,variable_names,data,iterations):
+# Learns a directed model MLE parameters, using the EM algorithm.
+def learn_directed_PGM_EM(prior_factors,data_variable_names,data,iterations):
 	old_factors = prior_factors
 	for iteration in range(iterations):
-		new_factors = [f.copy_zeros() for f in all_factors]
+		log_likelihood = 0
+		new_factors = [f.copy_zeros() for f in prior_factors]
 		for x in data:
-			known_names = [variable_names[v] for v in range(len(x)) if x[v]!=-1]
-			unknown_names = [variable_names[v] for v in range(len(x)) if x[v]==-1]
-			evidence = x[x!=-1]
-			if(len(unknown_names)>0):
-				infered_factor = sum_product_variable_elimination(old_factors,known_names,evidence,[])
-				for index in infered_factor.indexes:
-					probability = infered_factor.get(index)
-					evidence_filled = x.copy()
-					var_name_to_index = [variable_names.index(v) for v in infered_factor.names]
-					evidence_filled[np.array(var_name_to_index)]=index			
-					for j in range(len(new_factors)):
-						indexes = all_factors[j].indexes
-						factor_to_sample_index = [variable_names.index(name) for name in old_factors[j].names]
-						rearanged_index = evidence_filled[factor_to_sample_index]
+			known_names = [data_variable_names[v] for v in range(len(data_variable_names)) if x[v]!=-1]
+			known_evidence = x[x!=-1]
+			if(len(known_names)<len(data_variable_names)):
+				# E step: Infer the distribution over unknowns
+				infered_factor = sum_product_variable_elimination(old_factors,known_names,known_evidence,[])
+				# go through every possible option
+				for unknown_vector in infered_factor.indexes:
+					# get the full evidence vector, filling in the missing values.
+					infered_factor_to_sample_index_transform = [data_variable_names.index(v) for v in infered_factor.names]
+					full_evidence = x.copy()
+					full_evidence[infered_factor_to_sample_index_transform]=unknown_vector
+					probability = infered_factor.get(unknown_vector)
+					# For each factor update the counts at the given index.
+					for j in range(len(old_factors)):
+						# have to first make sure the indexing is the same as the order of variables can change. 
+						factor_to_sample_index_transform = [data_variable_names.index(name) for name in old_factors[j].names]
+						rearanged_index = full_evidence[factor_to_sample_index_transform]
 						old_value = new_factors[j].get(rearanged_index)
 						new_factors[j].set(rearanged_index,old_value+probability)
 			else:
-				for j in range(len(new_factors)):
-					indexes = all_factors[j].indexes
-					factor_to_sample_index = [variable_names.index(name) for name in old_factors[j].names]
-					rearanged_index = evidence[factor_to_sample_index]
+				for j in range(len(old_factors)):
+					factor_to_sample_index_transform = [data_variable_names.index(name) for name in old_factors[j].names]
+					rearanged_index = known_evidence[factor_to_sample_index_transform]
 					old_value = new_factors[j].get(rearanged_index)
 					new_factors[j].set(rearanged_index,old_value+1)
+			log_likelihood += get_log_likelihood(old_factors,known_names,known_evidence)
+		print("log likelihood",log_likelihood)
 		old_factors = [factors.condition(f,axis=f.names[1:]) for f in new_factors]
 	return old_factors
